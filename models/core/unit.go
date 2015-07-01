e2d48c33645f9a486a5e1fca806fbdd7bfaff8b7
Fang Yunlin <65141838@qq.com>
2015-07-01 14:05:03 +0800

    #添加了confd example。这是第一个基本可用版本。

--- models/core/unit.go
+++ models/core/unit.go
@@ -6,13 +6,13 @@ type Unit struct {
 	Image       string
 	Number      int64
 	Domain      string
-	Parameteres []*UnitParameter `orm:"reverse(many)"`
-	Dockerd     []*Dockerd       `orm:"rel(m2m)"`
+	Parameteres []*UnitParameter `orm:"reverse(many)"	json:"-"`
+	Dockerd     []*Dockerd       `orm:"rel(m2m)"	json:"-"`
 }
 
 type UnitParameter struct {
 	Id    int64
-	Unit  *Unit `orm:"rel(fk)"`
+	Unit  *Unit `orm:"rel(fk)"	json:"-"`
 	Value string
 	Type  string
 }
