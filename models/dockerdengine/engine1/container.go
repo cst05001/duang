427c6ed3cbd949c6f98f7b8ed0e1ae95aa010e09
Fang Yunlin <65141838@qq.com>
2015-07-03 09:59:48 +0800

    #reviewed models/dockerengine/engine1
    优化了日志输出格式

--- models/dockerdengine/engine1/container.go
+++ models/dockerdengine/engine1/container.go
@@ -1,13 +1,14 @@
 package engine1
 
+//review at 20150703
 import (
 	"fmt"
-	"regexp"
-
+	"github.com/astaxie/beego"
 	"github.com/cst05001/duang/models/core"
 	"github.com/cst05001/duang/models/dockerdengine"
 	"github.com/docker/docker/api/types"
 	docker "github.com/fsouza/go-dockerclient"
+	"regexp"
 )
 
 func (this *DockerClientEng1) Run(unit *core.Unit, callbackFunc func(*core.Dockerd, int, ...interface{})) error {
@@ -107,30 +108,31 @@ func (this *DockerClientEng1) Run(unit *core.Unit, callbackFunc func(*core.Docke
 			//第二个参数支持registry身份认证，还没处理。
 			err := client.PullImage(pullImageOptions, docker.AuthConfiguration{})
 			if err != nil {
-				fmt.Println(err)
+				beego.Error("Pull image ", pullImageOptions.Registry, pullImageOptions.Repository,
+					pullImageOptions.Tag, " at ", dockerd.GetIP(), " failed: ", err)
 				return
 			}
+			beego.Debug("Pull image ", pullImageOptions.Registry,
+				pullImageOptions.Repository, pullImageOptions.Tag, " at ", dockerd.GetIP(), " successed.")
 
 			container, err := client.CreateContainer(*createContainerOptions)
 			if err != nil {
-				fmt.Println(err)
+				beego.Error("Create container at ", dockerd.GetIP(), " failed: ", err)
 				containerCreateResponse.Warnings = append(containerCreateResponse.Warnings, err.Error())
 				callbackFunc(dockerd, dockerdengine.STATUS_ON_CREATE_FAILED, unit)
 				return
 			}
-			fmt.Println(container)
 			containerCreateResponse.ID = container.ID
 			callbackFunc(dockerd, dockerdengine.STATUS_ON_CREATE_SUCCESSED, unit)
 
 			// start container
-
-			fmt.Println(hostConfig.PortBindings)
 			err = client.StartContainer(containerCreateResponse.ID, hostConfig)
 			if err != nil {
-				fmt.Sprintf("StartContainer: Faile with error %s\n", err)
+				beego.Error("Start container at ", dockerd.GetIP(), " failed: ", err)
 				callbackFunc(dockerd, dockerdengine.STATUS_ON_RUN_FAILED, unit)
 				return
 			}
+			beego.Debug("StartContainer at ", dockerd.GetIP(), " successed")
 			callbackFunc(dockerd, dockerdengine.STATUS_ON_RUN_SUCCESSED, unit)
 		}()
 	}
